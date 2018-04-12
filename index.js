b495e0f5221f7974f2e573aea6ac1502c463e463
nyinyiwin <nyinyiwin87@gmail.com>
2018-04-12 23:02:08 +0630

    hello

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
+
 app.get('/',function(req,res) {
  res.send("<html><body>hello world.</body></html>");
     
 });
 
-var server = app.listen(5000,function(){
+var server = app.listen(8080,function(){
     console.log("server is running,");
 });
